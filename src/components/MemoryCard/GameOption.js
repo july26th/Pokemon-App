3fc8336db4e33741be0b2152e6f0da1e83d6a365
july26th <hien.tran.nmmt@gmail.com>
2019-10-23 18:08:40 +0700

    update

--- src/components/MemoryCard/GameOption.js
+++ src/components/MemoryCard/GameOption.js
@@ -14,32 +14,32 @@ class GameOption extends Component {
             <div className='container poke-box'>
                 <img src={gameTitle} alt=''/>
                 <div className="mode">
-                    <div class="ih-item square colored effect6 from_top_and_bottom">
+                    <div className="ih-item square colored effect6 from_top_and_bottom">
                         <Link to={{ pathname: '/game', state: { mode: 'easy' } }}>
                             <p className="img" style={{ backgroundImage: "url(" + easy + ")" }}></p>
 
                             <p className="text" style={{ border: "5px solid #fff400" }}>Easy</p>
-                            <div class="info">
+                            <div className="info">
                                 <h3>Easy Mode</h3>
                                 <h4>You have to find 6 pairs of pokemon in 20 turns</h4>
                             </div>
                         </Link>
                     </div>
-                    <div class="ih-item square colored effect6 from_top_and_bottom">
+                    <div className="ih-item square colored effect6 from_top_and_bottom">
                         <Link to={{ pathname: '/game', state: { mode: 'normal' } }}>
                             <p className="img" style={{ backgroundImage: "url(" + normal + ")" }}></p>
                             <p className="text" style={{ border: "5px solid #00c7ff" }}>Normal</p>
-                            <div class="info">
+                            <div className="info">
                                 <h3>Normal Mode</h3>
                                 <h4>You have to find 9 pairs of pokemon in 25 turns</h4>
                             </div>
                         </Link>
                     </div>
-                    <div class="ih-item square colored effect6 from_top_and_bottom">
+                    <div className="ih-item square colored effect6 from_top_and_bottom">
                         <Link to={{ pathname: '/game', state: { mode: 'hard' } }}>
                             <p className="img" style={{ backgroundImage: "url(" + hard + ")" }}></p>
                             <p className="text" style={{ border: "5px solid #ff0000" }}>hard</p>
-                            <div class="info">
+                            <div className="info">
                                 <h3>Hard Mode</h3>
                                 <h4>You have to find 12 pairs of pokemon in 30 turns</h4>
                             </div>
